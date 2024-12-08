b8a3f3ffd1c51cdd6eac1bd8b355de06235e6741
Deenank Sharma <146727112+DeenankSharma@users.noreply.github.com>
2024-12-08 10:17:22 +0530

    updating auth middleware

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,11 +2,20 @@ import jsonwebtoken from "jsonwebtoken";
 import dotenv from 'dotenv'
 import users from "../models/userModel.js";
 
-const JWT_SECRET = "FHHHHHVhsvhdhaee4456sjyjmjyohs6j86j2jshiJSHjsnjmd"
+const JWT_SECRET = ""
 
 dotenv.config()
 
 const requireAuth = async (req,res,next)=>{
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
     //verify authentication
     const {authorization} = req.headers
 
@@ -14,12 +23,31 @@ const requireAuth = async (req,res,next)=>{
         return res.status(401).json({error:"authorization token required!"})
     }
     const token  = authorization.split(' ')[1]
-
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
     try {
         const {id} = jsonwebtoken.verify(token,JWT_SECRET)
         req.user = await users.findOne({username:id},{_id:true})
         next()
     } catch (error) {
+            //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
+    //verify authentication
         console.log(error)
         res.status(401).json({error:"request is not authorised!"})
     }
